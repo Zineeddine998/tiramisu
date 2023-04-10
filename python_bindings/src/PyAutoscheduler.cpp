a0f7ebaf44ab96cc6fcf499de6dfd2c9400ce1b1
syphaxcheergui <is_chergui@esi.dz>
2023-04-10 19:24:11 +0400

    define_autoscheduler args fixed

--- python_bindings/src/PyAutoscheduler.cpp
+++ python_bindings/src/PyAutoscheduler.cpp
@@ -9,9 +9,9 @@ namespace tiramisu
         void define_autoscheduler(py::module &m)
         {
             m.def("create_and_run_autoscheduler",
-                  py::overload_cast<const std::vector<tiramisu::buffer *> &, std::function<int(halide_buffer_t *, halide_buffer_t *)>, const std::string, const std::string, int, int, tiramisu::function &>(&tiramisu::auto_scheduler::auto_scheduler::create_and_run_auto_scheduler),
+                  py::overload_cast<const std::vector<tiramisu::buffer *> &, const std::vector<halide_buffer_t *> &, std::function<int(halide_buffer_t *, halide_buffer_t *)>, const std::string, const std::string, int, int, tiramisu::function &>(&tiramisu::auto_scheduler::auto_scheduler::create_and_run_auto_scheduler),
                   "This function create and runs the autoscheduling process and print the results to a json file",
-                  py::arg("arguments"), py::arg("func"), py::arg("obj_filename"), py::arg("json_filename"), py::arg("beam_size"), py::arg("max_depth"), py::arg("fct"));
+                  py::arg("arguments"), py::arg("func_arguments"), py::arg("func"), py::arg("obj_filename"), py::arg("json_filename"), py::arg("beam_size"), py::arg("max_depth"), py::arg("fct"));
         }
 
     } // namespace PythonBindings
